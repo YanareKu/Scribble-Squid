db992a2ac14464a625d94e1921347902e53c9a43
Yanare Ku <lurial@gmail.com>
2015-03-02 18:03:56 -0800

    First Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from flask.ext.socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'TROLOLOLOLOLO!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connection')
+def listen_send_all(data):
+    emit('new user')
+
+@socketio.on('mousemove')
+def brdcast_moving(data):
+    emit('moving', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='127.0.0.1', port=5000)
+    
---
+++ static/js/script.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+
+	// Setting up all the globals
+    var canvas = document.getElementById('paper'),
+    ctx = canvas.getContext('2d') ? canvas.getContext('2d') : null,
+    url = 'http://localhost:5000',
+    socket = io.connect(url),
+    id = Math.round($.now()*Math.random()),
+    paint = false,
+    mode = "draw",
+    users = {},
+    cursors = {},
+    last_coord = {},
+    lastEmit = $.now();	
+
+    if (ctx === null) {
+      alert("You must use a browser that supports HTML5 Canvas to run this demo.");
+      return;
+    }
+
+	socket.on('moving', function (data) {	
+		if(! (data.id in users)){
+			cursors[data.id] = $('<p>').appendTo('#cursors');
+		}
+		
+		if (data.id != id) {
+			cursors[data.id].css({
+				'left' : data.x - this.offsetLeft,
+				'top' : data.y - this.offsetTop});
+		}
+
+		if(data.paint && users[data.id] && data.id != id){
+				makeStroke(users[data.id].x, users[data.id].y, data.x, data.y);
+		}
+
+		if(data.paint && users[data.id] && data.id != id && mode != "draw"){
+				eraser(users[data.id].x, users[data.id].y);
+		}
+		
+		users[data.id] = data; 
+		users[data.id].updated = $.now();
+	});
+
+	$(canvas).mousedown(function(e){
+		paint = true;
+		last_coord.x = e.pageX - this.offsetLeft;
+		last_coord.y = e.pageY - this.offsetTop;
+	});
+
+
+	$(canvas).mouseup(function(e){
+		paint = false;
+	});
+
+	$(canvas).mouseleave(function(e){
+		paint = false;
+	});
+
+	$(canvas).mousemove(function(e){
+		if($.now() - lastEmit > 30){
+			socket.emit('mousemove',{
+				'x': e.pageX - this.offsetLeft,
+				'y': e.pageY - this.offsetTop,
+				'paint': paint,
+				'mode': mode,
+				'id': id,
+			}); 
+			lastEmit = $.now();
+		}
+		
+		if(paint){
+			if(mode=="draw"){
+				makeStroke(last_coord.x, last_coord.y, e.pageX - this.offsetLeft, e.pageY - this.offsetTop); 
+			} else {
+				eraser(last_coord.x, last_coord.y);
+			}
+			last_coord.x = e.pageX - this.offsetLeft;
+			last_coord.y = e.pageY - this.offsetTop; 
+		}
+	});
+
+	$("#draw").click(function(){ 
+		mode="draw"; 
+	});
+
+	$("#eraser").click(function(){ 
+		mode="eraser"; 
+	});
+
+	setInterval(function(){
+		for(var ident in users){
+			if($.now() - users[ident].updated > 10000){
+				cursors[ident].remove();
+				delete users[ident];
+				delete cursors[ident];
+			}
+		}	
+	},10000);
+
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#1873b5';
+
+    function eraser(lastX, lastY){
+	    ctx.globalCompositeOperation="destination-out";
+		ctx.arc(lastX, lastY, 8, 0, Math.PI*2, false);
+	    ctx.fill();
+	}
+
+	function makeStroke(lastX, lastY, newX, newY){
+		ctx.moveTo(lastX, lastY);
+		ctx.lineTo(newX, newY);
+		ctx.stroke(); 
+	}
+
+});
