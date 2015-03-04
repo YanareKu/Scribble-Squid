b259536a13572dc3be8a2667181cd6df2c9a78f3
Yanare Ku <lurial@gmail.com>
2015-03-03 16:09:56 -0800

    Tried to make more explicit variable names. Broke eraser again. Will remove if can't figure it out.

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-	/*-----------------------------------------------------------
-	      				   Set Globals
-	-------------------------------------------------------------*/
+    /*-----------------------------------------------------------
+                           Set Globals
+    -------------------------------------------------------------*/
     var canvas = document.getElementById('paper'),
     ctx = canvas.getContext('2d') ? canvas.getContext('2d') : null,
     url = 'http://localhost:5000',
@@ -13,116 +13,105 @@ $(document).ready(function () {
     users = {},
     cursors = {},
     last_coord = {},
-    lastEmit = $.now();	
+    lastEmit = $.now(); 
 
     if (ctx === null) {
       alert("You must use a browser that supports HTML5 Canvas to run this demo.");
       return;
     }
 
-	/*-----------------------------------------------------------
-	      				Socket Events
-	-------------------------------------------------------------*/
-	socket.on('moving', function (data) {	
-		if(! (data.id in users)){
-			cursors[data.id] = $('<p>').appendTo('#cursors');
-		}
-		
-		if (data.id != id) {
-			cursors[data.id].css({
-				'left' : data.x - this.offsetLeft,
-				'top' : data.y - this.offsetTop});
-		}
+    /*-----------------------------------------------------------
+                    Socket Event // Remote Users
+    -------------------------------------------------------------*/
+    socket.on('moving', function (data) {   
+        if(!(data.remote_id in users)){
+            cursors[data.remote_id] = $('<p>').appendTo('#cursors');
+        }
+        
+        if (data.remote_id != id) {
+            cursors[data.remote_id].css({
+                'left' : data.remote_x - this.offsetLeft,
+                'top' : data.remote_y - this.offsetTop});
+        }
 
-		if(data.paint && users[data.id] && data.id != id && draw === true){
-				makeStroke(users[data.id].x, users[data.id].y, data.x, data.y);
-		}
+        if(data.remote_paint && users[data.remote_id] && data.remote_id != id && data.remote_draw === true){
+                makeStroke(users[data.remote_id].remote_x, users[data.remote_id].remote_y, data.remote_x, data.remote_y);
+        }
 
-		if(data.paint && users[data.id] && data.id != id && draw === false){
-				eraser(users[data.id].x, users[data.id].y);
-		}
+        if(data.remote_paint && users[data.remote_id] && data.remote_id != id && data.remote_draw === false){
+                eraser(users[data.remote_id].x, users[data.remote_id].y);
+        }
 
-		users[data.id] = data; 
-		users[data.id].updated = $.now();
-	});
+        users[data.remote_id] = data; 
+        users[data.remote_id].updated = $.now();
+    });
 
-	socket.on('erase_on', function (data) {	
-		if(data.id != id) {
-			draw = false;
-		}
-	});
+    /*----------------------------------------------
+                    Mouse Events
+    ------------------------------------------------*/
+    $(canvas).mousedown(function(e){
+        paint = true;
+        last_coord.x = e.pageX - this.offsetLeft;
+        last_coord.y = e.pageY - this.offsetTop;
+    });
 
-	socket.on('erase_off', function (data) {	
-		if(data.id != id) {
-			draw = true;
-		}
-	});
-	/*----------------------------------------------
-	      			Mouse Events
-	------------------------------------------------*/
-	$(canvas).mousedown(function(e){
-		paint = true;
-		last_coord.x = e.pageX - this.offsetLeft;
-		last_coord.y = e.pageY - this.offsetTop;
-	});
+    $(canvas).mouseup(function(e){
+        paint = false;
+    });
 
+    $(canvas).mouseleave(function(e){
+        paint = false;
+    });
 
-	$(canvas).mouseup(function(e){
-		paint = false;
-	});
+    $(canvas).mousemove(function(e){
+        if($.now() - lastEmit > 30){
+            socket.emit('mousemove',{
+                'remote_x': e.pageX - this.offsetLeft,
+                'remote_y': e.pageY - this.offsetTop,
+                'remote_paint': paint,
+                'remote_draw': draw,
+                'remote_id': id
+            }); 
+            lastEmit = $.now();
+        }
+        
+        if(paint){
+            if(draw){
+                makeStroke(last_coord.x, last_coord.y, e.pageX - this.offsetLeft, e.pageY - this.offsetTop); 
+            } else if(draw === false) {
+                eraser(last_coord.x, last_coord.y);
+            }
+            last_coord.x = e.pageX - this.offsetLeft;
+            last_coord.y = e.pageY - this.offsetTop; 
+        }
+    });
 
-	$(canvas).mouseleave(function(e){
-		paint = false;
-	});
+    /*----------------------------------------------
+                    Click Events
+    ------------------------------------------------*/  
+    $("#draw").click(function(){ 
+        draw = true; 
+        // socket.emit('draw',{
+        //     'draw': draw
+        // }); 
+    });
 
-	$(canvas).mousemove(function(e){
-		if($.now() - lastEmit > 30){
-			socket.emit('mousemove',{
-				'x': e.pageX - this.offsetLeft,
-				'y': e.pageY - this.offsetTop,
-				'paint': paint,
-				'id': id
-			}); 
-			lastEmit = $.now();
-		}
-		
-		if(paint){
-			if(draw){
-				makeStroke(last_coord.x, last_coord.y, e.pageX - this.offsetLeft, e.pageY - this.offsetTop); 
-			} else if(draw === false) {
-				eraser(last_coord.x, last_coord.y);
-			}
-			last_coord.x = e.pageX - this.offsetLeft;
-			last_coord.y = e.pageY - this.offsetTop; 
-		}
-	});
+    $("#eraser").click(function(){ 
+        draw = false;
+        // socket.emit('erase',{
+        //     'draw': draw
+        // }); 
+    });
 
-	/*----------------------------------------------
-	      			Click Events
-	------------------------------------------------*/	
-	$("#draw").click(function(){ 
-		draw = true; 
-		socket.emit('draw',{
-			'draw': draw
-		}); 
-	});
-
-	$("#eraser").click(function(){ 
-		draw = false;
-		socket.emit('erase',{
-			'draw': draw
-		}); 
-	});
-
-	setInterval(function(){
-		for(var ident in users){
-			if($.now() - users[ident].updated > 10000){
-				cursors[ident].remove();
-				delete users[ident];
-				delete cursors[ident];
-			}
-		}	
-	},10000);
+    setInterval(function(){
+        for(var ident in users){
+            if($.now() - users[ident].updated > 10000){
+                cursors[ident].remove();
+                delete users[ident];
+                delete cursors[ident];
+            }
+        }   
+    },10000);
 
     ctx.lineWidth = 5;
     ctx.lineJoin = 'round';
@@ -130,18 +119,18 @@ $(document).ready(function () {
     ctx.strokeStyle = '#1873b5';
 
     function eraser(lastX, lastY){
-	    ctx.globalCompositeOperation="destination-out";
-	    ctx.beginPath();
-		ctx.arc(lastX, lastY, 8, 0, Math.PI*2, false);
-	    ctx.fill();
-	}
+        ctx.globalCompositeOperation="destination-out";
+        ctx.beginPath();
+        ctx.arc(lastX, lastY, 8, 0, Math.PI*2, false);
+        ctx.fill();
+    }
 
-	function makeStroke(lastX, lastY, newX, newY){
-		ctx.globalCompositeOperation = "source-over";
-		ctx.beginPath();
-		ctx.moveTo(lastX, lastY);
-		ctx.lineTo(newX, newY);
-		ctx.stroke(); 
-	}
+    function makeStroke(lastX, lastY, newX, newY){
+        ctx.globalCompositeOperation = "source-over";
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(newX, newY);
+        ctx.stroke(); 
+    }
 
 });
